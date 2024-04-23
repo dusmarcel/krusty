cc0cc8f49ddd77416db1c623c4b79c4d059a2c29
marceldus <marcel@keienb.org>
2024-04-23 23:08:48 +0200

    added rel path for static

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -10,7 +10,7 @@ fn index() -> Template {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/static", FileServer::from(relative!("static")))
+        .mount("/static", FileServer::from(relative!("../static")))
         .mount("/pkg", FileServer::from(relative!("../frontend/pkg")))
         .mount("/", routes![index])
         .attach(Template::fairing())
