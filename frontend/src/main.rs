8244363b6989d2c551e420218567965237dc7808
marceldus <marcel@keienb.org>
2024-10-20 23:25:58 +0200

    added tailwind-, yew- and docker-stuff to the frontend

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <h1>{ "Hello, World!" }</h1>
+    }
+}
+
+fn main () {
+    yew::Renderer::<App>::new().render();
 }
