9caa099319fa8718a5d7068de69e6641cc6cd687
marceldus <marcel@keienb.org>
2024-10-25 00:21:09 +0200

    added rsa stuff, improved watching

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,14 @@
 #[macro_use] extern crate rocket;
 
+use openssl::rsa::Rsa;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> String {
+    let rsa = Rsa::generate(2048).unwrap();
+    match rsa.private_key_to_pem() {
+        Ok(v) => String::from_utf8(v).unwrap(),
+        Err(e) => format!("Couldn't read private key. The error message was: {}", e),
+    }
 }
 
 #[launch]
