e4e87db7ceb295c2c70345da69842b04467c0613
marceldus <marcel@keienb.org>
2024-11-23 23:49:43 +0100

    added backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/back")]
+async fn back() -> impl Responder {
+    HttpResponse::Ok().body("Hello, Krusty!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(back)
+    })
+    .bind(("127.0.0.1", 8161))?
+    .run()
+    .await
 }
--- frontend/build.rs
+++ frontend/build.rs
@@ -3,7 +3,7 @@ use std::{env, fs};
 fn main () {
     let backend_protocol=env::var("BACKEND_PROTOCOL").unwrap_or_else(|_| "http".to_string());
     let backend_host=env::var("BACKEND_HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
-    let backend_port=env::var("BACKEND_PORT").unwrap_or_else(|_| "8765".to_string());
+    let backend_port=env::var("BACKEND_PORT").unwrap_or_else(|_| "8161".to_string());
 
     let output=format!(r#"pub const BACKEND_URL: &str = "{}://{}:{}/back";"#, backend_protocol, backend_host, backend_port);
 
