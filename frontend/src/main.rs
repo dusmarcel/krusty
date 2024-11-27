aea267bd80b77a16f4c1f48a2d402ec814e2beda
marceldus <marcel@keienb.org>
2024-11-27 23:59:20 +0100

    added yew router

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,10 +1,19 @@
 include!("../backend_config.rs");
 use yew::prelude::*;
+use yew_router::prelude::*;
 use js_sys::JsString;
 use gloo_net::http::Request;
 
-#[function_component(App)]
-fn app() -> Html {
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/login")]
+    Login,
+}
+
+#[function_component(Home)]
+fn home() -> Html {
     let request_result = use_state(|| "No result yet. Try clicking on \"Get Actor\"".to_string());
     let result = request_result.clone();
 
@@ -25,6 +34,29 @@ fn app() -> Html {
             <button onclick={onclick}>{ "Get Actor" }</button>
             <p>{ format!("{}", *result) }</p>
         </>
+    }
+}
+
+#[function_component(Login)]
+fn login() -> Html {
+    html! {
+        <p>{ "Login" }</p>
+    }
+}
+
+fn switch(routes:Route) -> Html {
+    match routes {
+        Route::Home => html! { <Home /> },
+        Route::Login => html! { <Login /> },
+    }
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
     }
 }
 
