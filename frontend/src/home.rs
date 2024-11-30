2039087ce4bc67f52b6470f9ef1b56918a5a4186
marceldus <marcel@keienb.org>
2024-11-30 13:02:40 +0100

    refactoring frontend

---
+++ frontend/src/home.rs
@@ -0,0 +1,29 @@
+include!("../backend_config.rs");
+use yew::prelude::*;
+use js_sys::JsString;
+use gloo_net::http::Request;
+
+#[function_component(Home)]
+pub fn home() -> Html {
+    let request_result = use_state(|| "No result yet. Try clicking on \"Get Actor\"".to_string());
+    let result = request_result.clone();
+
+    let onclick = Callback::from(move |_| {
+        let result = request_result.clone();
+
+        wasm_bindgen_futures::spawn_local(async move {
+            let backend_url = BACKEND_URL.to_string();
+            let backend_msg = Request::get(&backend_url).send().await.unwrap().text().await.unwrap();
+            web_sys::console::log_1(&JsString::from(backend_msg.clone()));
+            result.set(backend_msg);
+        })
+    });
+
+    html! {
+        <>
+            <h1>{ "Hello, Krusty!" }</h1>
+            <button onclick={onclick}>{ "Get Actor" }</button>
+            <p>{ format!("{}", *result) }</p>
+        </>
+    }
+}
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,8 +1,7 @@
-include!("../backend_config.rs");
 use yew::prelude::*;
 use yew_router::prelude::*;
-use js_sys::JsString;
-use gloo_net::http::Request;
+
+use frontend::{home::Home, login::Login};
 
 #[derive(Clone, Routable, PartialEq)]
 enum Route {
@@ -10,38 +9,6 @@ enum Route {
     Home,
     #[at("/login")]
     Login,
-}
-
-#[function_component(Home)]
-fn home() -> Html {
-    let request_result = use_state(|| "No result yet. Try clicking on \"Get Actor\"".to_string());
-    let result = request_result.clone();
-
-    let onclick = Callback::from(move |_| {
-        let result = request_result.clone();
-
-        wasm_bindgen_futures::spawn_local(async move {
-            let backend_url = BACKEND_URL.to_string();
-            let backend_msg = Request::get(&backend_url).send().await.unwrap().text().await.unwrap();
-            web_sys::console::log_1(&JsString::from(backend_msg.clone()));
-            result.set(backend_msg);
-        })
-    });
-
-    html! {
-        <>
-            <h1>{ "Hello, Krusty!" }</h1>
-            <button onclick={onclick}>{ "Get Actor" }</button>
-            <p>{ format!("{}", *result) }</p>
-        </>
-    }
-}
-
-#[function_component(Login)]
-fn login() -> Html {
-    html! {
-        <p>{ "Login" }</p>
-    }
 }
 
 fn switch(routes:Route) -> Html {
