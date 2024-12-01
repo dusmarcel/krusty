37daf40787f068b3ad613bce8cc6162d3b8d46ea
marceldus <marcel@keienb.org>
2024-12-01 15:13:11 +0100

    added effect to home

--- frontend/src/home.rs
+++ frontend/src/home.rs
@@ -8,6 +8,18 @@ pub fn home() -> Html {
     let request_result = use_state(|| "No result yet. Try clicking on \"Say hello!\"".to_string());
     let result = request_result.clone();
 
+    {
+        let result = request_result.clone();
+        use_effect(move || {
+            wasm_bindgen_futures::spawn_local(async move {
+                let backend_url = BACKEND_URL.to_string();
+                let backend_msg = Request::get(&backend_url).send().await.unwrap().text().await.unwrap();
+                web_sys::console::log_1(&JsString::from(backend_msg.clone()));
+                result.set(backend_msg);
+            })            
+        });
+    }
+
     let onclick = Callback::from(move |_| {
         let result = request_result.clone();
 
