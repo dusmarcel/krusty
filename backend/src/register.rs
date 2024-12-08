fa448e1187b27429525239412a851a95f65d413f
marceldus <marcel@keienb.org>
2024-12-08 14:49:06 +0100

    added password hasher

--- backend/src/register.rs
+++ backend/src/register.rs
@@ -1,4 +1,5 @@
 use actix_web::{post, web, Responder};
+use argon2::{Argon2, PasswordHasher};
 use password_hash::{rand_core::OsRng, SaltString};
 use serde::Deserialize;
 
@@ -7,6 +8,7 @@ struct FormData {
     username: String,
     password: String
 }
+
 #[post("/back/register")]
 async fn register(form: web::Form<FormData>) -> impl Responder {
     println!("username: {}", form.username);
@@ -14,6 +16,10 @@ async fn register(form: web::Form<FormData>) -> impl Responder {
 
     let salt = SaltString::generate(&mut OsRng);
     println!("salt: {}", salt);
+
+    let argon2 = Argon2::default();
+    let hash = argon2.hash_password(form.password.as_bytes(), &salt).unwrap();
+    println!("{}", hash);
 
     web::Redirect::to("/register").see_other()
 }
