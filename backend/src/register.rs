487b89b0d5bf7146335140828d7a065da4597dc7
marceldus <marcel@keienb.org>
2024-12-08 13:26:26 +0100

    let's get salty

--- backend/src/register.rs
+++ backend/src/register.rs
@@ -1,4 +1,5 @@
 use actix_web::{post, web, Responder};
+use password_hash::{rand_core::OsRng, SaltString};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -10,6 +11,9 @@ struct FormData {
 async fn register(form: web::Form<FormData>) -> impl Responder {
     println!("username: {}", form.username);
     println!("password: {}", form.password);
+
+    let salt = SaltString::generate(&mut OsRng);
+    println!("salt: {}", salt);
 
     web::Redirect::to("/register").see_other()
 }
