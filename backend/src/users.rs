bdd0e3d1662ec3b023f3040a7221d270b48ed819
marceldus <marcel@keienb.org>
2024-12-11 22:51:17 +0100

    bug hunting

--- backend/src/users.rs
+++ backend/src/users.rs
@@ -1,11 +1,14 @@
+use std::sync::Mutex;
+
 use actix_web::{get, web, Responder, Result};
 
 use crate::Backend;
 
 #[get("/back/users")]
-async fn users(backend: web::Data<Backend>) -> Result<impl Responder> {
+async fn users(backend: web::Data<Mutex<Backend>>) -> Result<impl Responder> {
+    let my_backend = backend.lock().unwrap();
     let count: i64 = sqlx::query_scalar("SELECT COUNT(*) FROM users")
-        .fetch_one(&backend.pool)
+        .fetch_one(&my_backend.pool)
         .await
         .map_err(actix_web::error::ErrorInternalServerError)?;
 
