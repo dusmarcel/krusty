504260cd2c81946c78a91709a31bc567fadd7b9e
marceldus <marcel@keienb.org>
2024-12-16 23:10:34 +0100

    working on valkey stuff

--- backend/build.rs
+++ backend/build.rs
@@ -20,13 +20,20 @@ fn main () {
     let postgres_output=format!(r#"pub const POSTGRES_URL: &str = "{}";"#, postgres_url);
     fs::write("./postgres_config.rs", postgres_output).expect("Could'nt write postgres_config.rs");
 
+    // let valkey_host=env::var("VALKEY_HOST").unwrap_or_else(|_| "valkey".to_string());
+    // let valkey_port=env::var("VALKEY_PORT").unwrap_or_else(|_| "6379".to_string());
+    // let valkey_password=env::var("VALKEY_PASSWORD").unwrap_or_else(|_| "valkey".to_string());
+    // let valkey_url=format!("redis://{}:{}?auth={}",
+    //     valkey_host,
+    //     valkey_port,
+    //     valkey_password
+    // );
+
     let valkey_host=env::var("VALKEY_HOST").unwrap_or_else(|_| "valkey".to_string());
     let valkey_port=env::var("VALKEY_PORT").unwrap_or_else(|_| "6379".to_string());
-    let valkey_password=env::var("VALKEY_PASSWORD").unwrap_or_else(|_| "valkey".to_string());
-    let valkey_url=format!("redis://{}:{}?auth={}",
+    let valkey_url=format!("redis://{}:{}",
         valkey_host,
-        valkey_port,
-        valkey_password
+        valkey_port
     );
 
     let valkey_output=format!(r#"pub const VALKEY_URL: &str = "{}";"#, valkey_url);
