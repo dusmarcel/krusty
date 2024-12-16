ffd64f1be5f2a4c9c1d04181c3c4d58d4a2e27f2
marceldus <marcel@keienb.org>
2024-12-16 23:23:58 +0100

    seems to work now, now adding pw

--- backend/build.rs
+++ backend/build.rs
@@ -20,21 +20,21 @@ fn main () {
     let postgres_output=format!(r#"pub const POSTGRES_URL: &str = "{}";"#, postgres_url);
     fs::write("./postgres_config.rs", postgres_output).expect("Could'nt write postgres_config.rs");
 
-    // let valkey_host=env::var("VALKEY_HOST").unwrap_or_else(|_| "valkey".to_string());
-    // let valkey_port=env::var("VALKEY_PORT").unwrap_or_else(|_| "6379".to_string());
-    // let valkey_password=env::var("VALKEY_PASSWORD").unwrap_or_else(|_| "valkey".to_string());
-    // let valkey_url=format!("redis://{}:{}?auth={}",
-    //     valkey_host,
-    //     valkey_port,
-    //     valkey_password
-    // );
-
     let valkey_host=env::var("VALKEY_HOST").unwrap_or_else(|_| "valkey".to_string());
     let valkey_port=env::var("VALKEY_PORT").unwrap_or_else(|_| "6379".to_string());
-    let valkey_url=format!("rediss://{}:{}",
+    let valkey_password=env::var("VALKEY_PASSWORD").unwrap_or_else(|_| "valkey".to_string());
+    let valkey_url=format!("redis://{}:{}?auth={}",
         valkey_host,
-        valkey_port
+        valkey_port,
+        valkey_password
     );
+
+    // let valkey_host=env::var("VALKEY_HOST").unwrap_or_else(|_| "valkey".to_string());
+    // let valkey_port=env::var("VALKEY_PORT").unwrap_or_else(|_| "6379".to_string());
+    // let valkey_url=format!("rediss://{}:{}",
+    //     valkey_host,
+    //     valkey_port
+    // );
 
     let valkey_output=format!(r#"pub const VALKEY_URL: &str = "{}";"#, valkey_url);
     fs::write("./valkey_config.rs", valkey_output).expect("Could'nt write valkey_config.rs");    
