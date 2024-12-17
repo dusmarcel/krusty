a5a584f11a9716bb848d5f064b94e141a5ce543c
marceldus <marcel@keienb.org>
2024-12-17 20:45:13 +0100

    using char(32) for uuid instead of text

--- backend/migrations/20241217130028_init_users.up.sql
+++ backend/migrations/20241217130028_init_users.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 
 CREATE TABLE IF NOT EXISTS users (
-    uuid TEXT PRIMARY KEY,
+    uuid CHAR(32) PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
     salt TEXT NOT NULL,
     hash TEXT NOT NULL
