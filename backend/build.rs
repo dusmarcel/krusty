9231a09d8a84aeac6b8b741b5fefd2cc9c814849
marceldus <marcel@keienb.org>
2024-12-17 23:51:26 +0100

    still searching for bugs

--- backend/build.rs
+++ backend/build.rs
@@ -6,7 +6,7 @@ fn main () {
     let bc_output=format!(r#"pub const BACKEND_IF: &str = "{}";"#, backend_if);
     fs::write("./backend_config.rs", bc_output).expect("Could'nt write backend_config.rs");
 
-    let postgres_host=env::var("POSTGRES_HOST").unwrap_or_else(|_| "db".to_string());
+    let postgres_host=env::var("POSTGRES_HOST").unwrap_or_else(|_| "postgres".to_string());
     let postgres_database=env::var("POSTGRES_DATABASE").unwrap_or_else(|_| "postgres".to_string());
     let postgres_user=env::var("POSTGRES_USER").unwrap_or_else(|_| "postgres".to_string());
     let postgres_password=env::var("POSTGRES_PASSWORD").unwrap_or_else(|_| "postgres".to_string());
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -18,10 +18,12 @@ use backend::{
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let backend = Backend::new().await.map_err(|e| {
-        eprintln!("I cannot work under these conditions! Error while building backend: {}", e);
-        std::io::Error::new(std::io::ErrorKind::Other, format!("backend error: {}", e))        
-    })?;
+    let backend = Backend::new()
+        .await
+        .map_err(|e| {
+            eprintln!("I cannot work under these conditions! Error while building backend: {}", e);
+            std::io::Error::new(std::io::ErrorKind::Other, format!("backend error: {}", e))        
+        })?;
 
     let data = web::Data::new(Mutex::new(backend));
 
