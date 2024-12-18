241154f2e414466ef71280cd5fcab8200974b159
marceldus <marcel@keienb.org>
2024-12-18 09:27:37 +0100

    ...

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -28,12 +28,12 @@ async fn main() -> std::io::Result<()> {
     let data = web::Data::new(Mutex::new(backend));
 
     // let secret_key = Key::generate();
-    let redis_store = RedisSessionStore::new(VALKEY_URL)
-        .await
-        .map_err(|e| {
-            eprintln!("Could'nt create valkey/redis store! Error message: {}", e);
-            std::io::Error::new(std::io::ErrorKind::Other, format!("valkey/redis rror: {}", e))
-        })?;
+    // let redis_store = RedisSessionStore::new(VALKEY_URL)
+    //     .await
+    //     .map_err(|e| {
+    //         eprintln!("Could'nt create valkey/redis store! Error message: {}", e);
+    //         std::io::Error::new(std::io::ErrorKind::Other, format!("valkey/redis rror: {}", e))
+    //     })?;
 
     HttpServer::new(move || {
         App::new()
