169a9317f3d9ad01704acc6bd0af5dea7de6d8f7
marceldus <marcel@keienb.org>
2024-12-27 18:31:16 +0100

    fixing headers for host-meta

--- backend/src/well_known/host_meta.rs
+++ backend/src/well_known/host_meta.rs
@@ -1,6 +1,6 @@
 use std::sync::Mutex;
 
-use actix_web::{get, Responder, HttpResponse, web};
+use actix_web::{get, http::header::{ContentDisposition, ContentType}, web, HttpResponse, Responder};
 
 use crate::Backend;
 
@@ -8,17 +8,18 @@ use crate::Backend;
 async fn host_meta(backend: web::Data<Mutex<Backend>>) -> impl Responder {
     let my_backend = backend.lock().unwrap();
     if let Some(host) = &my_backend.host {
-        HttpResponse::Ok().body(
-            format!(
-                r#"
-                <?xml version="1.0"?>
-                <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">
-                    <Link rel="lrdd" template="https://{}/.well-known/webfinger?resource=acct:{{uri}}" type="application/json"/>
-                </XRD>
-                "#,
-                host
+        HttpResponse::Ok()
+            .content_type(ContentType::xml())
+            .insert_header(ContentDisposition::attachment("host-meta"))
+            .body(
+                format!(
+                    r#"<?xml version="1.0"?>
+                    <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">
+                        <Link rel="lrdd" template="https://{}/.well-known/webfinger?resource=acct:{{uri}}" type="application/json"/>
+                    </XRD>"#,
+                    host
+                )
             )
-        )
     } else {
         HttpResponse::InternalServerError().finish()
     }
