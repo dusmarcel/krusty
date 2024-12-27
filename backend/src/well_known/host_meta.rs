57612841b034de1b9e48c665cf942ad127ba6e80
marceldus <marcel@keienb.org>
2024-12-27 19:22:14 +0100

    cleanup

--- backend/src/well_known/host_meta.rs
+++ backend/src/well_known/host_meta.rs
@@ -1,6 +1,6 @@
 use std::sync::Mutex;
 
-use actix_web::{get, http::header::{ContentDisposition, ContentType}, web, HttpResponse, Responder};
+use actix_web::{get, web, HttpResponse, Responder};
 use mime;
 
 use crate::Backend;
@@ -12,12 +12,11 @@ async fn host_meta(backend: web::Data<Mutex<Backend>>) -> impl Responder {
         let mime = "application/xrd+xml; charset=utf-8".parse::<mime::Mime>().unwrap();
         HttpResponse::Ok()
             .content_type(mime)
-            //.insert_header(ContentDisposition::attachment("host-meta"))
             .body(
                 format!(
 r#"<?xml version="1.0"?>
 <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">
-    <Link rel="lrdd" template="https://{}/.well-known/webfinger?resource=acct:{{uri}}" type="application/json"/>
+    <Link rel="lrdd" template="https://{}/.well-known/webfinger?resource=acct:{{uri}}" />
 </XRD>
 "#,
                     host
